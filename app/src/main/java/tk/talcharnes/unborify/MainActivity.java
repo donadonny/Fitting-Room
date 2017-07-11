b0d33e0edff178221474d9850ab2472b578fd410
tal <tal32123@gmail.com>
2017-07-11 01:09:51 -0500

    Fix: PhotoUploadActivity now works completely!

--- app/src/main/java/tk/talcharnes/unborify/MainActivity.java
+++ app/src/main/java/tk/talcharnes/unborify/MainActivity.java
@@ -1,44 +1,19 @@
 package tk.talcharnes.unborify;
 
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.support.annotation.NonNull;
-import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
-
-import java.io.File;
-import java.io.IOException;
-
-import static tk.talcharnes.unborify.MainActivityFragment.REQUEST_IMAGE_CAPTURE;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
-    private StorageReference mStorageRef;
-    int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 1;
-    private String mCurrentPhotoPath;
-    Uri photoURI;
-    FirebaseDatabase database;
-    String imageFileNameNoJPG;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,9 +21,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        mStorageRef = FirebaseStorage.getInstance().getReference();
-        database = FirebaseDatabase.getInstance();
 
 
     }
@@ -82,119 +54,8 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void takePic(View view) throws IOException {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-        // Create the File where the photo should go
-        File photoFile = null;
-        try {
-            photoFile = getFile();
-        } catch (IOException ex) {
-            // Error occurred while creating the File
-
-        }
-        if (photoFile != null) {
-            photoURI = FileProvider.getUriForFile(this,
-                    "com.example.android.fileprovider",
-                    photoFile);
-            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-
-        }
-    }
-    private File getFile() throws IOException {
-        askForPermission();
-        Long timeStamp = System.currentTimeMillis();
-        imageFileNameNoJPG = FirebaseAuth.getInstance().getCurrentUser().getUid()+ "_AtTimeInMillis_" + timeStamp;
-
-
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileNameNoJPG,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-
-        mCurrentPhotoPath = image.getAbsolutePath();
-        return image;
-    }
-
-    private void askForPermission() {
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE)
-                    != PackageManager.PERMISSION_GRANTED) {
-
-                // Should we show an explanation?
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    if (shouldShowRequestPermissionRationale(
-                            android.Manifest.permission.READ_EXTERNAL_STORAGE)) {
-                        // Explain to the user why we need to read the contacts
-                    }
-                }
-
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    requestPermissions(new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE},
-                            MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
-                }
-
-                // MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE is an
-                // app-defined int constant that should be quite unique
-            }
-        }
-
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-
-            Toast.makeText(getApplicationContext(), "Uploading photo", Toast.LENGTH_SHORT).show();
-            StorageReference riversRef = mStorageRef.child("images/" + imageFileNameNoJPG);
-
-
-            if(mCurrentPhotoPath != null) {
-                UploadTask uploadTask =
-                        riversRef.putFile(photoURI);
-                uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                    @Override
-                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                        // Get a URL to the uploaded content
-                        Toast.makeText(getApplicationContext(), "Upload success!", Toast.LENGTH_SHORT).show();
-                        Uri downloadUrl = taskSnapshot.getDownloadUrl();
-
-                        Photo photo = new Photo();
-                        photo.setUrl(imageFileNameNoJPG);
-                        photo.setUser(FirebaseAuth.getInstance().getCurrentUser().getUid());
-                        photo.setLikes(0);
-                        photo.setDislikes(0);
-                        photo.setReports(0);
-                        photo.setOccasion_subtitle("TESTING APP");
-
-                        DatabaseReference photoReference = database.getReference("Photos").child(imageFileNameNoJPG);
-                        photoReference.setValue(photo);
-
-                        DatabaseReference userPhotosDatabaseReference = database.getReference("users")
-                                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
-                                .child(imageFileNameNoJPG);
-                        userPhotosDatabaseReference.setValue(imageFileNameNoJPG);
-                    }
-                })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception exception) {
-                                // Handle unsuccessful uploads
-                                Toast.makeText(getApplicationContext(), "Sending failed", Toast.LENGTH_SHORT).show();
-
-                                // ...
-                            }
-                        });
-            }
-            else{
-                Log.d(LOG_TAG, "mCurrentPhotoPath was null");
-            }
-
-        }
+    public void takePic(View view){
+        Intent intent = new Intent(this, PhotoUploadActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/tk/talcharnes/unborify/PhotoUploadActivityFragment.java
+++ app/src/main/java/tk/talcharnes/unborify/PhotoUploadActivityFragment.java
@@ -1,15 +1,57 @@
 package tk.talcharnes.unborify;
 
-import android.support.v4.app.Fragment;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.NavUtils;
+import android.support.v4.content.FileProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.io.IOException;
+
+import static android.app.Activity.RESULT_OK;
+import static tk.talcharnes.unborify.MainActivityFragment.REQUEST_IMAGE_CAPTURE;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class PhotoUploadActivityFragment extends Fragment {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private StorageReference mStorageRef;
+    int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 1;
+    private String mCurrentPhotoPath;
+    Uri photoURI;
+    String imageFileNameNoJPG;
+    FirebaseDatabase database;
+    ImageView userImageToUploadView;
+    boolean canUpload = false;
+    Button submitButton;
+    EditText photo_description_edit_text;
+    String photoDescription;
 
     public PhotoUploadActivityFragment() {
     }
@@ -17,6 +59,171 @@ public class PhotoUploadActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_photo_upload, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_photo_upload, container, false);
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+        database = FirebaseDatabase.getInstance();
+        photo_description_edit_text = (EditText) rootView.findViewById(R.id.photo_description_edit_text);
+
+
+        userImageToUploadView = (ImageView) rootView.findViewById(R.id.uploadedPhoto);
+        userImageToUploadView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    takePhoto();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        submitButton = (Button) rootView.findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean editTextNotNull = checkEditTextNotNull();
+                if(canUpload && editTextNotNull) {
+                    uploadPhoto();
+                }
+            }
+        });
+
+        return rootView;
+    }
+
+    private void takePhoto()throws IOException
+    {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // Create the File where the photo should go
+        File photoFile = null;
+        try {
+            photoFile = getFile();
+        } catch (IOException ex) {
+            // Error occurred while creating the File
+
+        }
+        if (photoFile != null) {
+            photoURI = FileProvider.getUriForFile(getContext(),
+                    "com.example.android.fileprovider",
+                    photoFile);
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+
+        }
+    }
+    private File getFile() throws IOException {
+        askForPermission();
+        Long timeStamp = System.currentTimeMillis();
+        imageFileNameNoJPG = FirebaseAuth.getInstance().getCurrentUser().getUid()+ "_AtTimeInMillis_" + timeStamp;
+
+
+        File storageDir = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileNameNoJPG,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void askForPermission() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (getActivity().checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+                // Should we show an explanation?
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (shouldShowRequestPermissionRationale(
+                            android.Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                        // Explain to the user why we need to read the contacts
+                    }
+                }
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    requestPermissions(new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE},
+                            MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                }
+
+                // MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE is an
+                // app-defined int constant that should be quite unique
+            }
+        }
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            canUpload = true;
+            Uri imageUri = photoURI;
+            Bitmap bitmap = null;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), imageUri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            userImageToUploadView.setImageBitmap(bitmap);
+        }
+    }
+    private void uploadPhoto(){
+        submitButton.setVisibility(View.GONE);
+        StorageReference riversRef = mStorageRef.child("images/" + imageFileNameNoJPG);
+        if(mCurrentPhotoPath != null) {
+            UploadTask uploadTask =
+                    riversRef.putFile(photoURI);
+            uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    // Get a URL to the uploaded content
+                    Toast.makeText(getContext(), "Upload success!", Toast.LENGTH_SHORT).show();
+                    Uri downloadUrl = taskSnapshot.getDownloadUrl();
+
+                    Photo photo = new Photo();
+                    photo.setUrl(imageFileNameNoJPG);
+                    photo.setUser(FirebaseAuth.getInstance().getCurrentUser().getUid());
+                    photo.setLikes(0);
+                    photo.setDislikes(0);
+                    photo.setReports(0);
+                    photo.setOccasion_subtitle(photoDescription);
+
+                    DatabaseReference photoReference = database.getReference("Photos").child(imageFileNameNoJPG);
+                    photoReference.setValue(photo);
+
+                    DatabaseReference userPhotosDatabaseReference = database.getReference("users")
+                            .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                            .child(imageFileNameNoJPG);
+                    userPhotosDatabaseReference.setValue(imageFileNameNoJPG);
+                    NavUtils.navigateUpFromSameTask(getActivity());
+                }
+            })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                            // Handle unsuccessful uploads
+                            Toast.makeText(getContext(), "Sending failed", Toast.LENGTH_SHORT).show();
+                            submitButton.setVisibility(View.VISIBLE);
+                            // ...
+                        }
+                    });
+        }
+        else{
+            Log.d(LOG_TAG, "mCurrentPhotoPath was null");
+            Toast.makeText(getContext(), "Uh-Oh! Upload failed, please try again later!", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+    private boolean checkEditTextNotNull(){
+         photoDescription =  photo_description_edit_text.getText().toString();
+        if (photoDescription != null && !photoDescription.isEmpty() && !photoDescription.equals("")){
+            return true;
+        }
+        else {
+            photo_description_edit_text.setError("Occasion can not be empty");
+            return false;
+        }
     }
 }
