0b2fc5a249708a6d78aebe0218ed123743c8d0f7
tal <tal32123@gmail.com>
2017-07-11 00:07:16 -0500

    Feat: Create new activity for uploading photo

---
+++ app/src/main/java/tk/talcharnes/unborify/PhotoUploadActivity.java
@@ -0,0 +1,21 @@
+package tk.talcharnes.unborify;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class PhotoUploadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_upload);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
