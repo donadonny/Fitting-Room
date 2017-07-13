950833b9e44d0674c25ecf0835f5bb8bbd2acfa4
tal <tal32123@gmail.com>
2017-07-12 19:15:41 -0500

    Feat: Add my photos section

---
+++ app/src/main/java/tk/talcharnes/unborify/my_photos/MyPhotoAdapter.java
@@ -0,0 +1,81 @@
+package tk.talcharnes.unborify.my_photos;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import tk.talcharnes.unborify.Photo;
+import tk.talcharnes.unborify.R;
+
+/**
+ * Created by Tal on 7/12/2017.
+ */
+
+public class MyPhotoAdapter extends RecyclerView.Adapter<MyPhotoAdapter.ViewHolder> {
+    private List<Photo> mDataset;
+    private Context mContext;
+
+    // Provide a reference to the views for each data item
+        // Complex data items may need more than one view per item, and
+        // you provide access to all the views for a data item in a view holder
+        public static class ViewHolder extends RecyclerView.ViewHolder {
+            // each data item is just a string in this case
+             ImageView mImageView;
+             TextView likesCountView;
+             TextView dislikesCountView;
+
+            public ViewHolder(View v) {
+                super(v);
+                mImageView = v.findViewById(R.id.card_image_view);
+                likesCountView = v.findViewById(R.id.likes);
+                dislikesCountView = v.findViewById(R.id.dislikes);
+            }
+        }
+
+        // Provide a suitable constructor (depends on the kind of dataset)
+        public MyPhotoAdapter(List<Photo> myDataset, Context context) {
+            mDataset = myDataset;
+            mContext = context;
+        }
+
+        // Create new views (invoked by the layout manager)
+        @Override
+        public MyPhotoAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                       int viewType) {
+            // create a new view
+            View v =  LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.my_photo_card, parent, false);
+            // set the view's size, margins, paddings and layout parameters
+
+            ViewHolder vh = new ViewHolder(v);
+            return vh;
+        }
+
+        // Replace the contents of a view (invoked by the layout manager)
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            // - get element from your dataset at this position
+            // - replace the contents of the view with that element
+            holder.likesCountView.setText(""+ mDataset.get(position).getLikes());
+            holder.dislikesCountView.setText(""+ mDataset.get(position).getLikes());
+            holder.mImageView.setImageDrawable(mContext.getResources().getDrawable(R.drawable.smiley));
+        }
+
+        // Return the size of your dataset (invoked by the layout manager)
+        @Override
+        public int getItemCount() {
+            return mDataset.size();
+        }
+
+        @Override
+        public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+            super.onAttachedToRecyclerView(recyclerView);
+        }
+    }
+
--- app/src/main/java/tk/talcharnes/unborify/my_photos/MyPhotosFragment.java
+++ app/src/main/java/tk/talcharnes/unborify/my_photos/MyPhotosFragment.java
@@ -1,17 +1,26 @@
 package tk.talcharnes.unborify.my_photos;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
+import tk.talcharnes.unborify.Photo;
 import tk.talcharnes.unborify.R;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MyPhotosFragment extends Fragment {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    ArrayList<Photo> photoList;
 
     public MyPhotosFragment() {
     }
@@ -20,6 +29,27 @@ public class MyPhotosFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootview = inflater.inflate(R.layout.fragment_my_photos, container, false);
+        mRecyclerView = (RecyclerView) rootview.findViewById(R.id.my_photos_recyclerview);
+
+        mLayoutManager = new LinearLayoutManager(getContext());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        photoList = new ArrayList<>();
+        Photo photo = new Photo();
+        photo.setLikes(1);
+        photoList.add(photo);
+        Photo photo2 = new Photo();
+        photo2.setLikes(2);
+        photo2.setUrl("blah");
+
+        photoList.add(photo2);
+        Photo photo3 = new Photo();
+        photo3.setLikes(3);
+
+        photoList.add(photo3);
+
+        mAdapter = new MyPhotoAdapter(photoList, getContext());
+        mRecyclerView.setAdapter(mAdapter);
 
 
 
