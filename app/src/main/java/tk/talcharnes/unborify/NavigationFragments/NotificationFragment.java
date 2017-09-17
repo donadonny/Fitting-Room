52454744855045d66c21bec328d56c6d404992d1
tal <tal32123@gmail.com>
2017-09-16 22:56:11 -0500

    Feat: Add smart banner to notifications screen

--- app/src/main/java/tk/talcharnes/unborify/NavigationFragments/NotificationFragment.java
+++ app/src/main/java/tk/talcharnes/unborify/NavigationFragments/NotificationFragment.java
@@ -6,6 +6,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
 import tk.talcharnes.unborify.R;
 
 /**
@@ -14,10 +17,18 @@ import tk.talcharnes.unborify.R;
  */
 
 public class NotificationFragment extends Fragment {
+    private AdView mAdView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notification, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_notification, container, false);
+
+        mAdView = (AdView) rootView.findViewById(R.id.notification_adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+        return rootView;
     }
 }
