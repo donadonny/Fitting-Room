5a975f4d02ba273172dd367cf69b977511c12283
Mario <marzinx3@gmail.com>
2017-11-06 02:30:03 -0500

    added comments espersso test

---
+++ app/src/androidTest/java/tk/talcharnes/unborify/CommentsTest.java
@@ -0,0 +1,57 @@
+package tk.talcharnes.unborify;
+
+
+import android.support.test.espresso.action.ViewActions;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class CommentsTest {
+
+    @Before
+    public void init(){
+        mActivityRule.getActivity().getSupportFragmentManager().beginTransaction();
+    }
+
+    @Rule
+    public ActivityTestRule<CommentActivity> mActivityRule =
+            new ActivityTestRule<CommentActivity>(CommentActivity.class);
+
+
+    @Test
+    public void CheckIfOneCommentsSaves(){
+        onView(withId(R.id.comment_edittext)).perform(typeText("HELLO!"));
+        onView(withId(R.id.submit_comment_button)).perform(click());
+        onView(withId(R.id.comments_recyclerView)).check(matches(withText("HELLO!")));
+    }
+
+    @Test
+    public void SaveMultipleComentsCommentsSave() {
+        String demoText = "ABC";
+        char x = 'C';
+        for (int i = 0; i < 10; i++) {
+            onView(withId(R.id.comment_edittext)).perform(typeText(demoText + (x + i)));
+            onView(withId(R.id.submit_comment_button)).perform(click());
+            onView(withId(R.id.comments_recyclerView)).check(matches(withText(demoText + (x + i))));
+        }
+    }
+        @Test
+        public void performCommentsScroll(){
+            onView(withId(R.id.comments_recyclerView)).perform(ViewActions.swipeUp());
+        }
+}
+
---
+++ app/src/test/java/tk/talcharnes/unborify/CommentsTest.java
@@ -0,0 +1,11 @@
+package tk.talcharnes.unborify;
+
+import org.junit.Test;
+
+public class CommentsTest {
+
+    @Test
+    public void CommentsTest(){
+
+    }
+}
