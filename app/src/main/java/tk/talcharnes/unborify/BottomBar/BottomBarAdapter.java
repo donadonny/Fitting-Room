3dc3b776e06fd32220773fb09ad8ae4fa8ae9817
Khuram Chaudhry <kchaud2@pride.hofstra.edu>
2017-11-27 06:41:33 -0500

    refactor: Added comments to the BottomBar classes

--- app/src/main/java/tk/talcharnes/unborify/BottomBar/BottomBarAdapter.java
+++ app/src/main/java/tk/talcharnes/unborify/BottomBar/BottomBarAdapter.java
@@ -7,26 +7,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by khuramchaudhry on 9/29/17.
+ * Created by Khuram Chaudhry on 9/29/17.
+ *
+ * This class is an adapter that SmartFragmentStatePagerAdapter
+ * and holds the fragments for the bottom bar.
  */
 
 public class BottomBarAdapter extends SmartFragmentStatePagerAdapter {
+
     private final List<Fragment> fragments = new ArrayList<>();
 
+    /**
+     * Default Constructor.
+     */
     public BottomBarAdapter(FragmentManager fragmentManager) {
         super(fragmentManager);
     }
 
-    // Our custom method that populates this Adapter with Fragments
+    /**
+     * This function is a custom method that populates this Adapter with Fragments.
+     */
     public void addFragments(Fragment fragment) {
         fragments.add(fragment);
     }
 
+    /**
+     * This function returns the fragment for the given position.
+     */
     @Override
     public Fragment getItem(int position) {
         return fragments.get(position);
     }
 
+    /**
+     * This function returns the number of fragments.
+     */
     @Override
     public int getCount() {
         return fragments.size();
