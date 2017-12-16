50221a0b1edee8db7b2266d054bd3d5d7a19675d
Mario <marzinx3@gmail.com>
2017-12-16 16:00:47 -0500

    worked on base page of deals

--- app/src/main/java/tk/talcharnes/unborify/MainNavigationFragments/DealsFragment.java
+++ app/src/main/java/tk/talcharnes/unborify/MainNavigationFragments/DealsFragment.java
@@ -1,11 +1,17 @@
 package tk.talcharnes.unborify.MainNavigationFragments;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import tk.talcharnes.unborify.Models.DealsAdapter;
 import tk.talcharnes.unborify.R;
 
 /**
@@ -13,8 +19,10 @@ import tk.talcharnes.unborify.R;
  * This fragment displays the deals screen.
  */
 
-public class DealsFragment extends Fragment {
-
+public class DealsFragment extends Fragment{
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
     private static final String TAG = DealsFragment.class.getSimpleName();
 
     @Override
@@ -22,7 +30,14 @@ public class DealsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_deals, container, false);
-
+        layoutManager = new LinearLayoutManager(this.getContext());
+        recyclerView.setLayoutManager(layoutManager);
+        List<String> input = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            input.add("Test" + i);
+        }// define an adapter
+        mAdapter = new DealsAdapter(input);
+        recyclerView.setAdapter(mAdapter);
         return rootView;
     }
 
