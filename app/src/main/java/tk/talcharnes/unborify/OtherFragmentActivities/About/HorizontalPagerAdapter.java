0fcc94425038bc868fb09b08708c377a7383cd9a
Khuram Chaudhry <earthfire2@gmail.com>
2018-03-08 16:31:12 -0500

    sytle: Changed the about screen UI

--- app/src/main/java/tk/talcharnes/unborify/OtherFragmentActivities/About/HorizontalPagerAdapter.java
+++ app/src/main/java/tk/talcharnes/unborify/OtherFragmentActivities/About/HorizontalPagerAdapter.java
@@ -1,40 +1,38 @@
 package tk.talcharnes.unborify.OtherFragmentActivities.About;
 
 import android.content.Context;
-import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v4.view.PagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
-import com.google.firebase.storage.StorageReference;
+import android.widget.TextView;
 import java.util.ArrayList;
 import tk.talcharnes.unborify.R;
-import tk.talcharnes.unborify.PhotoCard.ZoomPhotoActivity;
-import tk.talcharnes.unborify.Utilities.StorageConstants;
 
 /**
  * Created by Khuram Chaudhry on 10/23/17.
- * This PagerAdapter holds and sets the card views for the user's likes and favorites list.
+ * This PagerAdapter holds and sets the card views for the about screen.
  */
 
 public class HorizontalPagerAdapter extends PagerAdapter {
 
     private Context mContext;
     private LayoutInflater mLayoutInflater;
-    private ArrayList<String> urls;
+    private int[] texts;
+    private int[] colorsActive;
 
-    public HorizontalPagerAdapter(final Context context, ArrayList<String> urls) {
+    public HorizontalPagerAdapter(final Context context, int[]texts) {
         mContext = context;
         mLayoutInflater = LayoutInflater.from(context);
-        this.urls = urls;
+        this.texts = texts;
+        colorsActive = context.getResources().getIntArray(R.array.array_dot_active);
+
     }
 
     @Override
     public int getCount() {
-        return urls.size();
+        return texts.length;
     }
 
     @Override
@@ -44,8 +42,8 @@ public class HorizontalPagerAdapter extends PagerAdapter {
 
     @Override
     public Object instantiateItem(@NonNull final ViewGroup container, final int position) {
-        View view = mLayoutInflater.inflate(R.layout.card_view, container, false);
-        setupItem(view, urls.get(position));
+        View view = mLayoutInflater.inflate(R.layout.card_about, container, false);
+        setupItem(view, position);
 
         container.addView(view);
         return view;
@@ -62,20 +60,10 @@ public class HorizontalPagerAdapter extends PagerAdapter {
         container.removeView((View) object);
     }
 
-    private void setupItem(final View view, final String url) {
-        final ImageView img = (ImageView) view.findViewById(R.id.img_item);
-        final ProgressBar progressBar = (ProgressBar) view.findViewById(R.id.progress_bar);
-        final StorageReference storageReference = StorageConstants.getImageRef(url + ".webp");
-        img.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(mContext, ZoomPhotoActivity.class);
-                intent.putExtra("url", url + ".webp");
-                intent.putExtra("rotation", 0);
-                mContext.startActivity(intent);
-            }
-        });
-        StorageConstants.loadImageUsingGlide(mContext, img, storageReference, progressBar);
+    private void setupItem(final View view, final int position) {
+        final TextView textView = (TextView) view.findViewById(R.id.text_item);
+        textView.setText(texts[position]);
+        textView.setBackgroundColor(colorsActive[position%colorsActive.length]);
 
     }
 }
